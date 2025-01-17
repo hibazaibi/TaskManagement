f3ffc20e973ae870822b72b7000100db3af1787d
hibazaibi <hibazaibi00@gmail.com>
2025-01-17 17:33:03 +0100

    aaa

--- TaskManagement/Program.cs
+++ TaskManagement/Program.cs
@@ -3,7 +3,6 @@ using TaskManagement.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -18,11 +17,9 @@ builder.Services.AddAuthentication("CookieAuth")
     });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
